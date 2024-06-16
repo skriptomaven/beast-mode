a24353200c6bacb87602bfd1c5a25c95360995eb
skriptomaven <bygeoffreyouma@gmail.com>
2024-06-16 23:59:12 +0300

    Improve spelling

--- python/data_structures_and_algorithms.py
+++ python/data_structures_and_algorithms.py
@@ -1,6 +1,12 @@
-# Data Structures and Algorithms
+'''
+    ============================
+    DATA STRUCTURES & ALGORITHMS
+    ============================
 
-''' An algorithm: A set of step-by-step instructions to solve any given problem.
+    ---------
+    Algorithm
+    ---------
+        A set of step-by-step instructions to solve any given problem.
         - Algorithm processes the data and produces the output results based on the specific problem
         - The data used by the algorithm to solve the problem has to be stored & organized efficiently
         in the computer memory for the efficient implementation of the software.
@@ -10,7 +16,9 @@
         - Data structures deal with how the data is stored & organized in the memory  of the computer
         that is going to be used in the program.
 
-    Data Types:
+    ----------
+    Data Types
+    ----------
         These are simple data types
         1. Numeric - Integer(int), float, complex
         2. Boolean - bool
