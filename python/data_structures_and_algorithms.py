1d2745ce2d75db634da2b0fc3f0a772b0535e228
skriptomaven <bygeoffreyouma@gmail.com>
2024-06-16 23:45:10 +0300

    A little more about lists

--- python/data_structures_and_algorithms.py
+++ python/data_structures_and_algorithms.py
@@ -37,4 +37,39 @@
                 stupid_list = [2, 'Kayla', 'diel', 700]
              - Lists are mutable: elements can be: added, deleted, shifted, and moved within
              the list.
+
+             - Properties of lists:
+                - ordered
+                - dynamic
+                - list element can be an arbirtray set of objects
+                - list element can be accessed through indexing: a[2]
+                - lists also support slicing: a[2:3]
+                - lists are mutable: elements can be updated by + or -
+                - list operators: in, not in, +, *, lenc(), max(), min()
+
+    ===========================================
+    MEMBERSHIP, IDENTITY, and LOGICAL OPERATORS
+    ===========================================
+
+    1. Membership Ops - are used to test for an element membership in a sequence: ```in``` and ```not in```
+        - in: tests if a value exists in a sequence.
+            : returns ```True``` if it does and ```False``` if it does not.
+
+            first_list = [1, 2, 3, 4]
+            second_list = [7, 8, 9]
+
+            if first_list[0] in second_list:
+                print('Found ya!')
+            else:
+                print('Not found')
+
+        - not in: returns ```True``` if an item is not in a list and ```False``` if found.
+
+            target_suspect = 23
+            list_of_suspects = [20, 2, 43, 100]
+
+            if target_suspect not in list_of_suspects:
+                print('Not here!)
+            else:
+                print('He is here!')
 '''
