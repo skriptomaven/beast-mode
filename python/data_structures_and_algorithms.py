015e21260f2342364b26bfcfade57cbc5a0ee78e
skriptomaven <bygeoffreyouma@gmail.com>
2024-06-16 20:18:16 +0300

    Explain what an algorithm is and how it relates with data, data structure, and computer memory

---
+++ python/data_structures_and_algorithms.py
@@ -0,0 +1,12 @@
+# Data Structures and Algorithms
+
+''' An algorithm: A set of step-by-step instructions to solve any given problem.
+        - Algorithm processes the data and produces the output results based on the specific problem
+        - The data used by the algorithm to solve the problem has to be stored & organized efficiently
+        in the computer memory for the efficient implementation of the software.
+        - The performance of the system depends upon the efficient access and retrieval of the data,
+        and that depends upon how well the data structures that store & organize the data in the
+        system are chosen.
+        - Data structures deal with how the data is stored & organized in the memory  of the computer
+        that is going to be used in the program.
+'''
